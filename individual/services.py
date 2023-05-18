bdb10ef1cf8d9bd96efdccd2488d69f87139a566
Kamil Malinowski <kmalinowski@soldevelo.com>
2023-05-18 15:06:13 +0200

    CM-17 Added service signals

--- individual/services.py
+++ individual/services.py
@@ -1,6 +1,7 @@
 import logging
 
 from core.services import BaseService
+from core.signals import register_service_signal
 from individual.models import Individual
 from individual.validation import IndividualValidation
 
@@ -8,6 +9,18 @@ logger = logging.getLogger(__name__)
 
 
 class IndividualService(BaseService):
+    @register_service_signal('individual_service.create')
+    def create(self, obj_data):
+        return super().create(obj_data)
+
+    @register_service_signal('individual_service.update')
+    def update(self, obj_data):
+        return super().update(obj_data)
+
+    @register_service_signal('individual_service.delete')
+    def delete(self, obj_data):
+        return super().delete(obj_data)
+
     OBJECT_TYPE = Individual
 
     def __init__(self, user, validation_class=IndividualValidation):
