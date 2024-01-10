d71aff124d45a0ef4f5d8a5aa629d342c0766a4c
Jan <jdolkowski@soldevelo.com>
2024-01-10 15:49:55 +0100

    CM-405: block task if another task in group (#45)
    * CM-405: block task if another task in group
    
    * CM-405: move super().validate
    
    ---------
    
    Co-authored-by: Jan <j.dolkowski@soldevelo.com>

--- individual/validation.py
+++ individual/validation.py
@@ -1,7 +1,11 @@
 from django.utils.translation import gettext as _
 from django.core.exceptions import ValidationError
+from django.db.models import Q
+from django.contrib.contenttypes.models import ContentType
+
 from individual.models import Individual, IndividualDataSource, GroupIndividual, Group
 from core.validation import BaseModelValidation
+from tasks_management.models import Task
 
 
 class IndividualValidation(BaseModelValidation):
@@ -16,6 +20,7 @@ class GroupValidation(BaseModelValidation):
     OBJECT_TYPE = Group
 
     def validate_update_group_individuals(cls, user, **data):
+        super().validate_update(user, **data)
         errors = []
         allowed_fields = {'id', 'individual_ids'}
         extra_fields = set(data.keys()) - allowed_fields
@@ -34,8 +39,39 @@ class GroupValidation(BaseModelValidation):
         if errors:
             raise ValidationError(errors)
 
-        super().validate_update(user, **data)
-
 
 class GroupIndividualValidation(BaseModelValidation):
     OBJECT_TYPE = GroupIndividual
+
+    @classmethod
+    def validate_update(cls, user, **data):
+        errors = [
+            *validate_group_task_pending(data)
+        ]
+        if errors:
+            raise ValidationError(errors)
+
+
+def validate_group_task_pending(data):
+    group_id = data.get('group_id')
+    content_type_groupindividual = ContentType.objects.get_for_model(GroupIndividual)
+    content_type_group = ContentType.objects.get_for_model(Group)
+    groupindividual_ids = list(GroupIndividual.objects.filter(group_id=group_id).values_list('id', flat=True))
+
+    is_groupindividual_task = Task.objects.filter(
+        Q(status=Task.Status.RECEIVED) | Q(status=Task.Status.ACCEPTED),
+        entity_type=content_type_groupindividual,
+        entity_id__in=groupindividual_ids,
+    ).exists()
+
+    is_group_task = Task.objects.filter(
+        Q(status=Task.Status.RECEIVED) | Q(status=Task.Status.ACCEPTED),
+        entity_type=content_type_group,
+        entity_id=group_id,
+    ).exists()
+
+    if is_groupindividual_task or is_group_task:
+        return [{"message": _("individual.validation.validate_group_task_pending") % {
+            'group_id': group_id
+        }}]
+    return []
