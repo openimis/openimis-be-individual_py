80997e4a83cfe092333cdff3fe451d3862d341fc
Wei Lu <luwei.here@gmail.com>
2024-09-05 11:35:27 -0400

    Add village to individual and group model
    to facilitate row security

--- individual/models.py
+++ individual/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
 
 import core
 from core.models import HistoryModel
-
-from django.utils.translation import gettext_lazy as _
+from graphql import ResolveInfo
+from location import models as location_models
+from location.models import LocationManager
 
 
 class Individual(HistoryModel):
@@ -13,8 +16,43 @@ class Individual(HistoryModel):
     #TODO WHY the HistoryModel json_ext was not enough
     json_ext = models.JSONField(db_column="Json_ext", blank=True, default=dict)
 
+    village = models.ForeignKey(
+        location_models.Location,
+        models.DO_NOTHING,
+        blank=True,
+        null=True
+    )
+
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+
+    @classmethod
+    def filter_queryset(cls, queryset=None):
+        if queryset is None:
+            queryset = cls.objects.all()
+        return queryset
+
+    @classmethod
+    def get_queryset(cls, queryset, user):
+        queryset = cls.filter_queryset(queryset)
+        if isinstance(user, ResolveInfo):
+            user = user.context.user
+        if settings.ROW_SECURITY and user.is_anonymous:
+            return queryset.filter(id=-1)
+        if settings.ROW_SECURITY and not user.is_imis_admin:
+            return queryset.filter(
+                models.Q(
+                    LocationManager().build_user_location_filter_query(
+                        user._u,
+                        prefix='village__parent__parent',
+                        loc_types=['D']
+                    ) | LocationManager().build_user_location_filter_query(
+                        user._u,
+                        prefix='family__village__parent__parent',
+                        loc_types=['D']
+                    )
+                )
+            )
 
     class Meta:
         managed = True
@@ -54,6 +92,33 @@ class IndividualDataUploadRecords(HistoryModel):
 class Group(HistoryModel):
     code = models.CharField(max_length=64, blank=False, null=False)
     json_ext = models.JSONField(db_column="Json_ext", blank=True, default=dict)
+    village = models.ForeignKey(
+        location_models.Location,
+        models.DO_NOTHING,
+        blank=True,
+        null=True
+    )
+
+    @classmethod
+    def filter_queryset(cls, queryset=None):
+        if queryset is None:
+            queryset = cls.objects.all()
+        return queryset
+
+    @classmethod
+    def get_queryset(cls, queryset, user):
+        queryset = cls.filter_queryset(queryset)
+        if isinstance(user, ResolveInfo):
+            user = user.context.user
+        if settings.ROW_SECURITY and user.is_anonymous:
+            return queryset.filter(id=-1)
+        if settings.ROW_SECURITY and not user.is_imis_admin:
+            return queryset.filter(
+                LocationManager().build_user_location_filter_query(
+                    user._u, prefix='village__parent__parent', loc_types=['D']
+                )
+            )
+        return queryset
 
 
 class GroupDataSource(HistoryModel):
